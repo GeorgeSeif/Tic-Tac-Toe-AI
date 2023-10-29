e7576f1564e199c8172c0275f677f76ab46ca743
ansafronov <asafronov@nes.ru>
2023-10-29 19:12:01 +0000

    pretify repository

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,3 @@
 cd build 
 make test
-
-# echo " -- Running Test"
-# # Compile from stdin. - signifies stdin. -xc++ specifies language is c++
-# g++ src/test.cpp -Wall -Wextra -o build/gtesttest -std=${CXX_STANDARD} -lgtest -lpthread 
-
-# ./build/gtesttest
-
-# echo " -- Test Success!"
-
-# TODO: поменять на make test <=> добавить в cmake
+# ctest -V
--- test.sh
+++
@@ -1,1 +0,0 @@
-echo jhluibn | ./test
